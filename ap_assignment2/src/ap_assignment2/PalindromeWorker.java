9ba3f7768925599a138413bd5bca814559cdd684
Mirza <demon6656@gmail.com>
2017-12-27 06:58:08 +0500

    redundant comments removed

--- ap_assignment2/src/ap_assignment2/PalindromeWorker.java
+++ ap_assignment2/src/ap_assignment2/PalindromeWorker.java
@@ -10,11 +10,11 @@ import java.util.ArrayList;
 
 public class  PalindromeWorker {
 	
-	// Data members.
+
 	protected ArrayList<String> bag;
 	protected ArrayList<String> sharedList;
 	private static int count = 0;
-	// Constructor. 
+	
 	public PalindromeWorker(ArrayList<String> bag, ArrayList<String> sharedList) {
 		this.bag = bag; 
 		this.sharedList = sharedList;
@@ -22,33 +22,26 @@ public class  PalindromeWorker {
 	
 	public synchronized void findPalindromes() {
 		
-		
 		// iterate through every word of the "bag" ArrayList. 
-		try {
-			
+		try {	
 			for (int i = 0; i < bag.size(); i ++) {
-				
+
 				StringBuilder temp = new StringBuilder(bag.get(i));
 				temp = temp.reverse();
-				
+
 				// case1: the word is a classical palindrome (i.e its reversed version is the same).
 				// case2: the word is palindromic (reversed version exists in the "bag "ArrayList").
-				// note: both cases are true. Hence words are dumped in the "sharedList" each time.
-				
-				
+
 				if (bag.get(i).equals(temp.toString())  ) {
-					
+
 					sharedList.add(bag.get(i) + "\n" );
 					count++;
-					
 				}
 				else if (bag.contains(temp.toString())) {
 					sharedList.add(temp + "\n" );
 					count++;
 				}
-				
 			}
-			
 		}
 		
 		catch (Exception e) {
